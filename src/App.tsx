5eb9b819e7c20378395622fd6752265193f7ee20
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2022-08-07 14:04:47 +0700

    Add nav and note model

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-function App() {
-  return <div>Hello World</div>
+const App: React.FC = () => {
+  return (
+    <div>
+      <nav>
+        <h1 className='bg-slate-300 p-4 text-2xl font-bold'>Notes</h1>
+      </nav>
+    </div>
+  )
 }
 
 export default App
---
+++ src/model/Note.ts
@@ -0,0 +1,7 @@
+export interface NoteModel {
+  id: number
+  title: string
+  body: string
+  createdAt: Date
+  archived: boolean
+}
